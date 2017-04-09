fe1596abfae9f9cf6d72e0817e26d590667e0311
Christopher Gauffin <christopher.gauffin@gmail.com>
2017-04-09 18:25:03 +0200

    first commit

---
+++ .aliases.sh
@@ -0,0 +1,9 @@
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias grep='grep --color'
+
+#alias mkcd='_mkcd(){ mkdir "$1" && cd "$1"; }; _mkcd'
+alias update='sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove && sudo apt autoclean'
+alias sizmekvpn='sudo openconnect vpn.mediamind.com --authgroup SIZMEK_ISRAEL -u christopher.gauffin'
