e5c91b4ddeedd18ca12e849e0f636284cdd811ca
Christopher Gauffin <christopher.gauffin@gmail.com>
2017-08-22 20:18:08 +0200

    more removal

--- .config/polybar/mail.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import imaplib
-obj = imaplib.IMAP4_SSL('imap.gmail.com',993)
-obj.login('christopher.gauffin@gmail.com','0M3lagow6n') # write your email and password
-obj.select('Primary')
-print(len(obj.search(None, 'UnSeen')[1][0].split()))
