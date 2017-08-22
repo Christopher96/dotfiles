d9c8d357e4e29d165376122f401ec2acd7dd5546
Christopher Gauffin <christopher.gauffin@gmail.com>
2017-08-22 23:17:06 +0200

    removed dangerous code from functions.sh

--- .functions.sh
+++ .functions.sh
@@ -1,68 +1,28 @@
 function cdl(){
-  cd $1
-  ls
+    cd $1
+    ls
 }
 
 function mkcd(){
-  mkdir $1
-  cd $1
+    mkdir $1
+    cd $1
 }
 
 function apmy(){
-  sudo systemctl $1 apache2
-  sudo systemctl $1 mysql
+    sudo systemctl $1 apache2
+    sudo systemctl $1 mysql
 }
 
 urlencode() {
-  local i= char= url=$*
-  declare -i len=${#url}
+    local i= char= url=$*
+    declare -i len=${#url}
 
-  for (( i = 0; i < len; i++ )); do
-    char=${url:i:1}
-    case "$char" in
-      [a-zA-Z0-9.~_-]) printf "$char" ;;
-      ' ') printf + ;;
-      *) printf '%%%X' "'$char" ;;
-    esac
-  done
-}
-
-github-create() {
- repo_name=$1
-
- dir_name=`basename $(pwd)`
-
- if [ "$repo_name" = "" ]; then
- echo "Repo name (hit enter to use '$dir_name')?"
- read repo_name
- fi
-
- if [ "$repo_name" = "" ]; then
- repo_name=$dir_name
- fi
-
- username=`git config github.user`
- if [ "$username" = "" ]; then
- echo "Could not find username, run 'git config --global github.user <username>'"
- invalid_credentials=1
- fi
-
- token=`git config github.token`
- if [ "$token" = "" ]; then
- echo "Could not find token, run 'git config --global github.token <token>'"
- invalid_credentials=1
- fi
- 
- if [ "$invalid_credentials" = 1 ]; then
- return 1
- fi
- 
- echo -n "Creating Github repository '$repo_name' ..."
- curl -u "$username:$token" https://api.github.com/user/repos -d '{"name":"'$repo_name'"}' > /dev/null 2>&1
- echo " done."
-
- echo -n "Pushing local code to remote ..."
- git remote add origin git@github.com:$username/$repo_name.git > /dev/null 2>&1
- git push -u origin master > /dev/null 2>&1
- echo " done."
+    for (( i = 0; i < len; i++ )); do
+      char=${url:i:1}
+      case "$char" in
+	[a-zA-Z0-9.~_-]) printf "$char" ;;
+	' ') printf + ;;
+	*) printf '%%%X' "'$char" ;;
+      esac
+    done
 }
